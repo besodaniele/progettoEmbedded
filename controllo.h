5a4d68faa510441ebc41a3347e6acf9d163289ef
Leslyz3024 <164195423+Leslyz3024@users.noreply.github.com>
2025-06-06 09:01:09 +0200

    modifiche lab

--- controllo.h
+++ controllo.h
@@ -1,116 +1,104 @@
+#ifndef CONTROLLO_H
+#define CONTROLLO_H
 #include <stdio.h>
 #include <stdlib.h>
-//#include <pigpio.h>
+#include <pigpio.h>
 #include "libcoderbot/include/motor.h"
 #include "libcoderbot/include/cbdef.h"
 #include "libcoderbot/include/encoder.h"
 #include "concurrent.h"
-#define _USE_MATH_DEFINES
+#include "constants.h"
 #include <math.h>
-#ifndef M_PI
-	#define M_PI 3.14159265358979323846
-#endif
 
-#define KP 0.005f//da trovare
-#define KI 0.0005f//da trovare
+extern pthread_mutex_t clamp_mutex;     //mutex di clamp counter
 
-#define PERIOD 20 //in millisecondi, ergo 50Hz
+float clamp(float dutyCycle,int* clamp_counter){
+        mutex_lock(&clamp_mutex);
 
-#define CLAMP_LIMIT 30 //limite superiore al numero di saturazioni, in caso fermo tutto e.g. mi sono schiantato
+        if (*clamp_counter>=CLAMP_LIMIT){
+                mutex_unlock(&clamp_mutex);
+                exit(EXIT_FAILURE);
+        }
+        if (dutyCycle > 1.0f) {
+                *clamp_counter++;
+                mutex_unlock(&clamp_mutex);
+                return 1.0f;
+        }
+        else if (dutyCycle < 0.0f){
+                *clamp_counter++;
+                mutex_unlock(&clamp_mutex);
+                return 0.1f;
+        }
 
-#define WHEEL_RAY_MM 33.f
-#define TICKS_PER_REVOLUTION 16
-#define TRANSMISSION_RATIO 120
-const float mmsPerTick = (WHEEL_RAY_MM * 2 * M_PI) / (TICKS_PER_REVOLUTION * TRANSMISSION_RATIO);
-
-extern int clamp_counter /*= 0*/; //incrementata da entrambi i task del controllo dei motori, da proteggere con mutex
-extern pthread_mutex_t clamp_mutex;	//mutex di clamp counter
-
-float clamp(float dutyCycle){
-	mutex_lock(&clamp_mutex);
-
-	if (clamp_counter>=CLAMP_LIMIT){
-		mutex_unlock(&clamp_mutex);
-		exit(EXIT_FAILURE);
-	} 
-	if (dutyCycle > 1.0f) {
-		clamp_counter++;
-		mutex_unlock(&clamp_mutex);
-		return 1.0f;
-	}
-	else if (dutyCycle < 0.0f){
-		clamp_counter++;
-		mutex_unlock(&clamp_mutex);
-		return 0.1f;
-	}
-
-	mutex_unlock(&clamp_mutex);
-	return dutyCycle;
+        mutex_unlock(&clamp_mutex);
+        return dutyCycle;
 }
 
 typedef struct {
-	float goal_mm;
-	float targetSpeed_mm_s;
-	float dutyCycle;
-	cbMotor_t motor;
-	cbEncoder_t encoder;
+        float goal_mm;
+        float targetSpeed_mm_s;
+        float dutyCycle;
+        cbMotor_t* motor;
+        cbEncoder_t* encoder;
+        int* clamp_counter;
 } controllo_args_t;
 
-void controllo(void *args){
-	controllo_args_t *controlArgs = (controllo_args_t *)args;
-    float goal_mm = controlArgs->goal_mm;
-    cbMotor_t motor = controlArgs->motor;
-    float dutyCycle = controlArgs->dutyCycle;
-	float targetSpeed_mm_s = controlArgs->targetSpeed_mm_s;
-	cbEncoder_t encoder = controlArgs->encoder;
-    //set_sched_deadline(80*1000,15*1000,200*1000);
-    // da stabilire i valori
+void* controllo(void *args){
+        controllo_args_t *controlArgs = (controllo_args_t *)args;
+        float goal_mm = controlArgs->goal_mm;
+        cbMotor_t motor = *(controlArgs->motor);
+        float dutyCycle = controlArgs->dutyCycle;
+        float targetSpeed_mm_s = controlArgs->targetSpeed_mm_s;
+        cbEncoder_t encoder = *(controlArgs->encoder);
+        // da stabilire i valori
+        int* clamp_counter = controlArgs->clamp_counter;
 
-	int tick = 0;
-	int prevTick = 0;
-	float travelledDistance = 0.0f;
-	float speed = 0.0f;
-	float error = 0.0f;
-	float newDutyCycle = 0.0f;
-	float erroreAccumulatoL = 0.0f, erroreAccumulatoR = 0.0f;
+        set_sched_deadline(15*1000*1000,19*1000*1000,20*1000*1000);
 
-	cbDir_t direction=forward;
+        int tick = 0;
+        int prevTick = 0;
+        float travelledDistance = 0.0f;
+        float speed = 0.0f;
+        float error = 0.0f;
+        float newDutyCycle = 0.0f;
+        float erroreAccumulatoL = 0.0f, erroreAccumulatoR = 0.0f;
 
-	cbMotorMove(&motor, direction, dutyCycle);
+        cbDir_t direction=forward;
 
-	while(goal_mm > 0.0f){
+        cbMotorMove(&motor, direction, dutyCycle);
 
-		tick = encoder.ticks;
+        while(1){
 
-		travelledDistance = (tick - prevTick)*mmsPerTick;
+                tick = encoder.ticks;
+                printf("%d\n",tick);
+                travelledDistance = (tick - prevTick)*mmsPerTick;
 
-		prevTick = tick;
+                prevTick = tick;
 
-		speed = (travelledDistance / PERIOD) * 1000; // il periodo è in millisecondi, per avere la velocità in mm/s mi riporto ai secondi
+                speed = (travelledDistance / PERIOD) * 1000; // il periodo è in millisecondi, per avere la velocità in mm/s mi riporto ai secondi
 
-		error = targetSpeed_mm_s - speed;
+                error = targetSpeed_mm_s - speed;
+                erroreAccumulatoL += error; //sorrenti dice che nell'integrale va considerato l'errore corrente, non so quanto mi convince
 
-		erroreAccumulatoL += error; //sorrenti dice che nell'integrale va considerato l'errore corrente, non so quanto mi convince
-		
-		newDutyCycle = KP*error + KI*erroreAccumulatoL;
-		newDutyCycle = fabs(newDutyCycle);
+                newDutyCycle = KP*error + KI*erroreAccumulatoL;
+                newDutyCycle = fabs(newDutyCycle);
 
-		//controllo saturazione
-		newDutyCycle = clamp(newDutyCycle);
-		// se errore positivo -> troppo lento
-		// se error negativo -> troppo veloce
+                //controllo saturazione
+                newDutyCycle = clamp(newDutyCycle,clamp_counter);
+                // se errore positivo -> troppo lento
+                // se error negativo -> troppo veloce
 
-		if (error > 0){
-			direction = forward;
-		}else if (error < 0){
-			direction = backward;
-			newDutyCycle = newDutyCycle *0.1f;
-			newDutyCycle = clamp(newDutyCycle);
-		}
-		cbMotorMove(&motor, direction, newDutyCycle);
-		goal_mm -= travelledDistance;
-        pthread_testcancel();
-        sched_yield(); //segnalo allo scheduler che ho finito
-	}
+                if (error > 0){
+                        direction = forward;
+                }else if (error < 0){
+                        direction = backward;
+                        newDutyCycle = newDutyCycle *0.1f;
+                        newDutyCycle = clamp(newDutyCycle,clamp_counter);
+                }
+                cbMotorMove(&motor, direction, newDutyCycle);
+                goal_mm -= travelledDistance;
+                pthread_testcancel();
+                sched_yield(); //segnalo allo scheduler che ho finito
+        }
 }
-
+#endif
